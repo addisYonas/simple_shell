5b53cc66b8a3be3f161d6da533f957e17a049c52
adonay <samueldesta323@gmail.com>
2022-06-15 14:21:45 -0700

    added 2 files :

---
+++ cd_shell_fun.c
@@ -0,0 +1,74 @@
+#include "main.h"
+
+/**
+ * cd_shell - changes current directory
+ *
+ * @datash: data relevant
+ * Return: 1 on success
+ */
+int cd_shell(data_shell *datash)
+{
+	char *dir;
+	int ishome, ishome2, isddash;
+
+	dir = datash->args[1];
+
+	if (dir != NULL)
+	{
+		ishome = _strcmp("$HOME", dir);
+		ishome2 = _strcmp("~", dir);
+		isddash = _strcmp("--", dir);
+	}
+
+	if (dir == NULL || !ishome || !ishome2 || !isddash)
+	{
+		cd_to_home(datash);
+		return (1);
+	}
+
+	if (_strcmp("-", dir) == 0)
+	{
+		cd_previous(datash);
+		return (1);
+	}
+
+	if (_strcmp(".", dir) == 0 || _strcmp("..", dir) == 0)
+	{
+		cd_dot(datash);
+		return (1);
+	}
+
+	cd_to(datash);
+
+	return (1);
+}
+
+/**
+ * exit_shell - exits the shell
+ *
+ * @datash: data relevant (status and args)
+ * Return: 0 on success.
+ */
+int exit_shell(data_shell *datash)
+{
+	unsigned int ustatus;
+	int is_digit;
+	int str_len;
+	int big_number;
+
+	if (datash->args[1] != NULL)
+	{
+		ustatus = _atoi(datash->args[1]);
+		is_digit = _isdigit(datash->args[1]);
+		str_len = _strlen(datash->args[1]);
+		big_number = ustatus > (unsigned int)INT_MAX;
+		if (!is_digit || str_len > 10 || big_number)
+		{
+			get_error(datash, 2);
+			datash->status = 2;
+			return (1);
+		}
+		datash->status = (ustatus % 256);
+	}
+	return (0);
+}
