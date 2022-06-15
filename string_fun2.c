92478ecba488e3df2780ac11374fb600318405c8
addis yonas <addyonas@gmail.com>
2022-06-15 14:13:33 -0700

    betty fix

--- string_fun2.c
+++ string_fun2.c
@@ -59,6 +59,13 @@ int cmp_chars(char str[], const char *delim)
 		return (1);
 	return (0);
 }
+
+/**
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
+ */
+
 int _atoi(char *s)
 {
 	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
