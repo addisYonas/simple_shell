320ae3a5424c9f2e55a6166a3c043c76b7068143
addis yonas <addyonas@gmail.com>
2022-06-15 14:04:30 -0700

    add string function

--- string_fun1.c
+++ string_fun1.c
@@ -14,7 +14,7 @@ char *_strcat(char *dest, const char *src)
 	for (i = 0; dest[i] != '\0'; i++)
 		;
 
-	for (j = 0; src[j] != '\0'; j++)file:
+	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
 		i++;
